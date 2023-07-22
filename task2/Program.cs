26ad60a4c7de807b76e25795dd56ec0086d51fa7
Snake <stop@mail.ru>
2023-07-23 00:31:09 +0300

    Задача 2 решена

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите первое число: ");
+int first=int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int second=int.Parse(Console.ReadLine()!);
+if(first>second) Console.WriteLine($"Первое число {first} больше второго числа {second}.");
+else if(first<second) Console.WriteLine($"Второе число {second} больше первого числа {first}.");
+else if(first==second) Console.WriteLine($"Числа {second} и {first} равны.");
