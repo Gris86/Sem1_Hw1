66d0b315af1f085bae55b2cb90bd9d40bc27d862
Snake <stop@mail.ru>
2023-07-23 01:07:45 +0300

    Задание 4 выполнено

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+int first=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int second=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int third=int.Parse(Console.ReadLine()!);
+int max=first;
+if(second > max) max = second;
+if(third > max)  max = third;
+if(max==second && max==third) {
+     Console.WriteLine($"Числа {first}, {second} и {third} равны.");}
+else if(max!=second || max!=third) {
+Console.WriteLine($"Из чисел {first}, {second} и {third} максимальное {max}.");}
