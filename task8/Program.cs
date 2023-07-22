1d63438ac0074b5866732dcbf3be8beb328ca3dc
Snake <stop@mail.ru>
2023-07-23 01:24:59 +0300

    Второй способ решения задачи 8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
-int calc=1;
+int calc=2;
 while(calc<=number){
-    if(calc%2==0) Console.Write($"{calc} ");
-    calc++;
+    Console.Write($"{calc} ");
+    calc+=2;
 }
